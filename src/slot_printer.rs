59730f44c25de2a6f5299d2a4d2e25974187f185
Arniiiii <lg3dx6fd@gmail.com>
2025-11-26 11:57:12 +0000

    add a slot printer

---
+++ src/slot_printer.rs
@@ -0,0 +1,24 @@
+// Copyright 2024 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+use cryptoki::context::{CInitializeArgs, Pkcs11};
+use cryptoki::mechanism::Mechanism;
+use cryptoki::object::Attribute;
+use cryptoki::session::UserType;
+use cryptoki::types::AuthPin;
+use std::env;
+
+fn main() -> eyre::Result<()> {
+    // initialize a new Pkcs11 object using the module from the env variable
+    let pkcs11 = Pkcs11::new(
+        env::var("TEST_PKCS11_MODULE")
+            .unwrap_or_else(|_| "/usr/lib64/softhsm/libsofthsm2.so".to_string()),
+    )?;
+
+    pkcs11.initialize(CInitializeArgs::OsThreads)?;
+
+    let slot = pkcs11.get_all_slots()?;
+
+    println!("slots: \n{:?}", slot);
+
+    Ok(())
+}
